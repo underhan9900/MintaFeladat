caa96f87d93c04bdeaf22f3efc95b1e6d7ba98a9
underhan9900 <jozsikak2001@gmail.com>
2026-09-14 10:05:39 +0200

    MintaFeladat Homework

--- masodik.py
+++ masodik.py
@@ -1,2 +1,6 @@
-felhasznalo_nev = "Józsi"
-print("Szia", felhasznalo_nev,'!')
+felhasznalo_kora = int(15.65)
+felhasznalo_kora += 1
+felhasznalo_neve = 'Józsi'
+felhasznalo_neve *= 2
+metszet = felhasznalo_neve[2]
+print('Szia', felhasznalo_neve, "!", felhasznalo_kora[2])
