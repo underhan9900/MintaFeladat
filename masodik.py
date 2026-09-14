9834b61d00cd76ad6861024b4a55ffb463eed1f0
underhan9900 <jozsikak2001@gmail.com>
2026-09-14 10:29:48 +0200

    MintaFeladat Homework

--- masodik.py
+++ masodik.py
@@ -1,13 +1,15 @@
 # Ez a második labor feladatait tartalmazza
 felhasznalo_kora = int(15.65)
+felhasznalo_kora = int(input("Hány éves vagy: "))
 felhasznalo_kora += 1
-felhasznalo_neve = 'Józsi'
+felhasznalo_neve = input('Kérem a nevet:')
 felhasznalo_neve *= 2
 metszet = felhasznalo_neve[:-5]
 jegyek = [2, 5, 4, 3]
 jegyek += [5]
 del jegyek[0]
 halmaz = {'magyar', 'angol', 'orosz', 3}
-elem = halmaz
-print(halmaz[0])
+hallgato = {"nev": 'Jolán', "kor": 19}
+print(hallgato["nev"])
+print(halmaz)
 print('Szia', felhasznalo_neve, "!", jegyek)
